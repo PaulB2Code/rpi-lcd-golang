44f2e2dd5eebc3efd966c5dd5b7a39d60c91ddcf
Your Name <berthomieu.paul@gmail.com>
2016-08-18 11:48:19 +0200

    Remove main in lcd

--- lcd.go
+++ lcd.go
@@ -323,19 +323,3 @@ func (l *Lcd) display(msg string) {
 		}
 	}
 } //display(msg string)
-
-func main() {
-
-	var disp Display
-	log.Printf("main: starting lcd")
-	disp = NewLcd()
-	disp.Display("Coucou     \n     yeah Go!")
-	defer func() {
-		if e := recover(); e != nil {
-			log.Printf("Recover: %v", e)
-		}
-		disp.Close()
-		log.Printf("main.defer: all closed")
-	}()
-	time.Sleep(1 * time.Second)
-} //main()
